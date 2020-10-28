1d75cc00e51e23a9811327a1264a1f3c0d0e1412
Yannickriou <yannick.riou@laas.fr>
2020-10-28 11:44:59 +0100

    Added drop task and modified move task to allow predefined poses

--- include/pr2_mtc/pr2_tasks/pr2_tasks.h
+++ include/pr2_mtc/pr2_tasks/pr2_tasks.h
@@ -8,6 +8,7 @@
 #include <moveit/task_constructor/task.h>
 #include <moveit/task_constructor/stages/current_state.h>
 #include <moveit/task_constructor/stages/generate_grasp_pose.h>
+#include <moveit/task_constructor/stages/generate_custom_pose.h>
 #include <moveit/task_constructor/stages/generate_place_pose.h>
 #include <moveit/task_constructor/stages/simple_grasp.h>
 #include <moveit/task_constructor/stages/pick.h>
@@ -17,6 +18,8 @@
 #include <moveit/task_constructor/stages/modify_planning_scene.h>
 #include <moveit/task_constructor/stages/compute_ik.h>
 
+#include <moveit/task_constructor/cost_terms.h>
+
 #include <moveit/task_constructor/solvers/pipeline_planner.h>
 #include <moveit/task_constructor/solvers/cartesian_path.h>
 #include <moveit/task_constructor/solvers/joint_interpolation.h>
@@ -25,9 +28,11 @@
 
 #include <moveit_visual_tools/moveit_visual_tools.h>
 
+
 // Moveit  related include
 #include <moveit/move_group_interface/move_group_interface.h>
 #include <moveit/planning_scene_interface/planning_scene_interface.h>
+#include <moveit/planning_scene_monitor/planning_scene_monitor.h>
 
 #include <moveit_msgs/CollisionObject.h>
 
@@ -64,6 +69,9 @@
 // Service to get object pose from UDWS
 #include <pr2_motion_tasks_msgs/GetPose.h>
 
+// Message to tell about facts
+#include <pr2_motion_tasks_msgs/StringStamped.h>
+
 // Action server for supervisor to call pick, place and move tasks
 #include <pr2_motion_tasks_msgs/planAction.h>
 #include <pr2_motion_tasks_msgs/executeAction.h>
@@ -79,19 +87,23 @@ class motionPlanning
         explicit motionPlanning();
         ~motionPlanning();
 
-        void createPlaceTask(Task &placeTask, const std::string planGroup, const std::string object, const geometry_msgs::PoseStamped placePose);
+        void createPlaceTask(Task &placeTask, const std::string planGroup, const std::string object, std::vector<geometry_msgs::PoseStamped> placePoses);
 
         void createMoveTask(Task &moveTask, const std::string planGroup, const geometry_msgs::PoseStamped moveToPose);
 
-        void createPickTaskCustom(Task &pickTask, const std::string planGroup,const std::string object, const geometry_msgs::PoseStamped graspPose);
+        void createMovePredefinedTask(Task &moveTask, const std::string planGroup,const std::string pose_id);
+
+        void createPickTaskCustom(Task &pickTask, const std::string planGroup,const std::string object, std::vector<geometry_msgs::PoseStamped> graspPoses);
 
         void createPickTask(Task &pickTask, const std::string planGroup,const std::string object);
+
+        void createDropTask(Task &dropTask, const std::string planGroup,const std::string object);
 
         int updateWorld(ros::ServiceClient& udwClient);
 
         void planCallback(const pr2_motion_tasks_msgs::planGoalConstPtr& goal,  actionlib::SimpleActionServer<pr2_motion_tasks_msgs::planAction>* planServer, ros::ServiceClient& udwClient);
 
-        void executeCallback(const pr2_motion_tasks_msgs::executeGoalConstPtr& goal,  actionlib::SimpleActionServer<pr2_motion_tasks_msgs::executeAction>* executeServer);
+        void executeCallback(const pr2_motion_tasks_msgs::executeGoalConstPtr& goal,  actionlib::SimpleActionServer<pr2_motion_tasks_msgs::executeAction>* executeServer, ros::Publisher factsPublisher);
 
 
 
@@ -108,6 +120,8 @@ class motionPlanning
         tf2_ros::Buffer tfBuffer_;
         tf2_ros::TransformListener transformListenner_;
 
+        // List to keep track of mesh/obj association to avoid asking for mesh
+        // if we already have it
         std::unordered_map<std::string, std::string> objMeshMap_;
 
         // Define PlanningSceneInterface object to add and remove collision objects
@@ -126,9 +140,9 @@ class motionPlanning
         // planner used for gripper open/close movements
         std::shared_ptr<solvers::JointInterpolationPlanner> gripper_planner_;
 
+        // Variable to set the eef used during task
+        std::string eef_;
 
-
-
-
-
+        // Variable to set the ikFrame used during task
+	    std::string ikFrame_;
 };
